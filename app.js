856f98302ebbe70bdb20c484dd30ec8a2f5e0e8e
Greg Nordeng <nordcoder@outlook.com>
2017-11-15 20:38:48 -0800

    Had to push before switching to Master

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var arrStores = []; //storing my store data into an array
+var openHours = ['6AM', '7AM', '8AM', '9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM', '6PM', '7PM']; //the hours each store is open
 
 function Store(location, minCust, maxCust, avgCookiePerCust) { //Cookie store constructor function
   this.location = location; // Store name
@@ -10,10 +11,10 @@ function Store(location, minCust, maxCust, avgCookiePerCust) { //Cookie store co
   this.avgCookiePerHour = 0; // function generated number taking a random number between maxCust and minCust and multiplying it by average cookie per customer
   this.hourlySales = []; // an hourly list of sales in that given hour
   this.totalSalesPerStore = 0; //Total cookie sales in a day per store will be set here
-  this.openHours = ['6AM', '7AM', '8AM', '9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM', '6PM', '7PM']; //the hours each store is open
-  arrStores.push(this);
+  arrStores.push(this);//creating an array of my instantiated objects
   this.findHourlySales();
   this.findTotalSalesPerStore();
+  this.buildTable();
 };
 
 //defining my methods
@@ -26,7 +27,7 @@ Store.prototype.findAvgCookiePerHour = function() { //Will use rng() and average
 };
 
 Store.prototype.findHourlySales = function() {//assign a number of sales per hour the store is open
-  for (var j = 0; j < this.openHours.length; j++) { //loop through total hours open within each store to create a value for the hourlySales array
+  for (var j = 0; j < openHours.length; j++) { //loop through total hours open within each store to create a value for the hourlySales array
     this.findAvgCookiePerHour();
     this.hourlySales[j] = Math.round(this.avgCookiePerHour); //use findAvgCookiePerHour() to update hourlySales array and round to nearest integer
   }
@@ -37,10 +38,16 @@ Store.prototype.findTotalSalesPerStore = function() { //function to add the tota
   var totalSalesPerStore = this.hourlySales.reduce(function(accumulator, currentValue) {
     return accumulator + currentValue;}); //something I found online to add all the integers in an array
   this.totalSalesPerStore = totalSalesPerStore; //updating each objects value for total sales in a day
-  console.log(arrStores);
   return this.totalSalesPerStore;
 };
-//instantiating my objects
+
+Store.prototype.buildTable = function() {
+  var tableEl = document.getElementById('store_table');
+  tableEl.appendChild(buildTableHead());
+  tableEl.appendChild(buildTableBody());
+};
+
+//instantiating my objects with hard data
 
 var firstPike = new Store('1st and Pike', 23, 65, 6.3);
 var seaTac = new Store('SeaTac Airport', 3, 24, 1.2);
@@ -48,30 +55,48 @@ var seattleCenter = new Store('Seattle Center', 11, 23, 3.7);
 var capitalHill = new Store('Capital Hill', 20, 38, 2.3);
 var alki = new Store('Alki', 20, 38, 4.6);
 
-//creating an array of my instantiated objects
+
 
 
 function rng(min, max) { //helper function to find a random number
   return Math.floor(Math.random() * (max - min)) + min;
 };
 
+console.log(arrStores);
 
+function buildTableHead() {
+  var theadEl = document.createElement('thead');
+  var headRow = buildTableRow('', openHours, 'Totals');
+  theadEl.appendChild(headRow);
+  return theadEl;
+}
 
-function buildingTable() { //my failng attempt to build a table, cant define the logic because it doesnt work and my brain hurts.
+function buildTableBody() {
+  var tbodyEl = document.createElement('tbody');
 
-  for (var i = 0; i < arrStores.length; i++) {
-    var table = document.getElementById('store_table');
-    var row = document.createElement('tr');
-    var column = document.createElement('td');
-    table.appendChild(row);
-    row.innerHTML = arrStores[i].openHours;
-    for (var j = 0; j < arrStores[i].hourlySales.length; j++) {
-      table.appendChild(column);
-      column.innerHTML = arrStores[i].hourlySales[j];
-    }
+  for(var i = 0; i < arrStores.length; i++) {
+    var bodyRow = buildTableBody(arrStores[i].name, arrStores[i].hourlySales, arrStores[i].totalSalesPerStore);
   }
-};
+  tbodyEl.appendChild(bodyRow);
+}
 
+function buildTableRow(yAxisHeader, data, yAxisFooter) {
+  var trEl = document.createElement('tr');
+  var tdElFirst = document.createElement('td');
+  tdElFirst.textContent = yAxisHeader;
+  trEl.appendChild(tdElFirst);
+
+  for (var i = 0; i < data.length; i++) {
+    var tdElSecond = document.createElement('td');
+    tdElSecond.textContent = data[i];
+    trEl.appendChild(tdElSecond);
+  }
+  var tdElThird = document.createElement('td');
+  tdElThird.textContent = yAxisFooter;
+  trEl.appendChild(tdElThird);
+
+  return trEl;
+}
 //fire off my functions in order
 
 //buildingTable();
