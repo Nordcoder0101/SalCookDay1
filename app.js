48c920a707982814eff745866e8ea0a6256105a7
Greg Nordeng <nordcoder@outlook.com>
2017-11-15 16:09:14 -0800

    refactored with working prototype functions, still needs table

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var arrStores = []; //storing my store data into an array
+
 function Store(location, minCust, maxCust, avgCookiePerCust) { //Cookie store constructor function
   this.location = location; // Store name
   this.minCust = minCust; // Min customers in 1 hour
@@ -9,8 +11,35 @@ function Store(location, minCust, maxCust, avgCookiePerCust) { //Cookie store co
   this.hourlySales = []; // an hourly list of sales in that given hour
   this.totalSalesPerStore = 0; //Total cookie sales in a day per store will be set here
   this.openHours = ['6AM', '7AM', '8AM', '9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM', '6PM', '7PM']; //the hours each store is open
+  arrStores.push(this);
+  this.findHourlySales();
+  this.findTotalSalesPerStore();
 };
 
+//defining my methods
+Store.prototype.findAvgCookiePerHour = function() { //Will use rng() and average cookie per customer to find average cookies sold per hour
+  for (var i = 0; i < arrStores.length; i++) {
+    var randomNumber = rng(this.maxCust, this.minCust); //assigning a var to my stores random generated customer per hour
+    this.avgCookiePerHour = Math.round(randomNumber * this.avgCookiePerCust); //arithmitic to find a random number of cookies sold in an hour
+  }
+  return this.avgCookiePerHour; //update value of arrStores
+};
+
+Store.prototype.findHourlySales = function() {//assign a number of sales per hour the store is open
+  for (var j = 0; j < this.openHours.length; j++) { //loop through total hours open within each store to create a value for the hourlySales array
+    this.findAvgCookiePerHour();
+    this.hourlySales[j] = Math.round(this.avgCookiePerHour); //use findAvgCookiePerHour() to update hourlySales array and round to nearest integer
+  }
+  return this.hourlySales;
+};
+
+Store.prototype.findTotalSalesPerStore = function() { //function to add the total number of sales made at each store in a day.
+  var totalSalesPerStore = this.hourlySales.reduce(function(accumulator, currentValue) {
+    return accumulator + currentValue;}); //something I found online to add all the integers in an array
+  this.totalSalesPerStore = totalSalesPerStore; //updating each objects value for total sales in a day
+  console.log(arrStores);
+  return this.totalSalesPerStore;
+};
 //instantiating my objects
 
 var firstPike = new Store('1st and Pike', 23, 65, 6.3);
@@ -20,41 +49,13 @@ var capitalHill = new Store('Capital Hill', 20, 38, 2.3);
 var alki = new Store('Alki', 20, 38, 4.6);
 
 //creating an array of my instantiated objects
-var arrStores = [firstPike, seaTac, seattleCenter, capitalHill, alki]; //storing my store data into an array
+
 
 function rng(min, max) { //helper function to find a random number
   return Math.floor(Math.random() * (max - min)) + min;
 };
 
-function findAvgCookiePerHour() { //Will use rng() and average cookie per customer to find average cookies sold per hour
-  for (var i = 0; i < arrStores.length; i++) {
-    var randomNumber = rng(arrStores[i].maxCust, arrStores[i].minCust); //assigning a var to my stores random generated customer per hour
-    arrStores[i].avgCookiePerHour = randomNumber * arrStores[i].avgCookiePerCust; //arithmitic to find a random number of cookies sold in an hour
-  }
-  return arrStores; //update value of arrStores
-};
 
-function findHourlySales() {//assign a number of sales per hour the store is open
-  for (var i = 0; i < arrStores.length; i++) { //loop through objects
-    for (var j = 0; j < arrStores[i].openHours.length; j++) { //loop through total hours open within each store to create a value for the hourlySales array
-      findAvgCookiePerHour();
-      arrStores[i].hourlySales[j] = Math.round(arrStores[i].avgCookiePerHour); //use findAvgCookiePerHour() to update hourlySales array and round to nearest integer
-    }
-  }
-  console.log(arrStores);
-  return arrStores;
-}
-
-function findTotalSalesPerStore() { //function to add the total number of sales made at each store in a day.
-  for (var i = 0; i < arrStores.length; i++) {
-    var totalSalesPerStore = arrStores[i].hourlySales.reduce(function(accumulator, currentValue) {
-      return accumulator + currentValue;
-    }); //something I found online to add all the integers in an array
-    arrStores[i].totalSalesPerStore = totalSalesPerStore; //updating each objects value for total sales in a day
-  }
-  console.log(arrStores);
-  return arrStores;
-};
 
 function buildingTable() { //my failng attempt to build a table, cant define the logic because it doesnt work and my brain hurts.
 
@@ -73,6 +74,4 @@ function buildingTable() { //my failng attempt to build a table, cant define the
 
 //fire off my functions in order
 
-findHourlySales();
-findTotalSalesPerStore();
-buildingTable();
+//buildingTable();
